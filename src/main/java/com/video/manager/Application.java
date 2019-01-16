64037cb5a652aa183499abb1f5ebf7cbf25d0fb4
liujianqiang <liujianqiang@yoho8.com>
2019-01-16 16:59:01 +0800

    打war包

--- src/main/java/com/video/manager/Application.java
+++ src/main/java/com/video/manager/Application.java
@@ -3,6 +3,7 @@ package com.video.manager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -19,6 +20,10 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
